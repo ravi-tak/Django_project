ac015ae33f7d17c549ec6d8d1061124dff9cf29f
RAVI KUMAR <ravikumartakajm@gmail.com>
2023-10-03 15:02:05 +0530

    deploy -v2

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'dj_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'NtrkVIcJYgJfHT4sk3Hs',
+        'HOST': 'containers-us-west-197.railway.app',
+        'PORT': '7503',
     }
 }
 
